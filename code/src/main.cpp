7f04a5ae3e6aabe404d371726cd22f70a3f7d4bc
Ken Koch <kkoch986@gmail.com>
2022-11-30 00:05:24 -0500

    chore: move CHANNEL_COUNT to the makefile
    just to make it easier to manage

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,8 +1,6 @@
 
-// 250 is the max packet size supproted by esp_now_send
-// save the first byte to indicate what page we are sending
+// 250 is the max packet size supported by esp_now_send
 #define MAX_BUFFER_SIZE 250
-#define CHANNEL_COUNT 512
 
 #ifndef RUN_MODE_CONTROLLER
   #ifndef RUN_MODE_RECEIVER
